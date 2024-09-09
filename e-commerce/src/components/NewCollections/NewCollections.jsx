12b33e6def9147dbe7b7091994f90d099edcdfb2
wadill <ewomaodiri@gmail.com>
2024-09-09 13:56:21 +0100

    car item; navbar, newsletter, offers, popular and related products function

--- e-commerce/src/components/NewCollections/NewCollections.jsx
+++ e-commerce/src/components/NewCollections/NewCollections.jsx
@@ -1,63 +1,20 @@
-import { Button } from "@/components/ui/button";
-import protokit from "@/public/protokit-zinc.svg";
-import Image from "next/image";
-// @ts-ignore
-import truncateMiddle from "truncate-middle";
-import { Skeleton } from "@/components/ui/skeleton";
-import { Chain } from "./chain";
-import { Separator } from "./ui/separator";
+import React from 'react'
+import './NewCollections.css'
+import new_collection from '../Assets/new_collections'
+import Item from '../Item/Item'
 
-export interface HeaderProps {
-  loading: boolean;
-  wallet?: string;
-  onConnectWallet: () => void;
-  balance?: string;
-  balanceLoading: boolean;
-  blockHeight?: string;
-}
-
-export default function Header({
-  loading,
-  wallet,
-  onConnectWallet,
-  balance,
-  balanceLoading,
-  blockHeight,
-}: HeaderProps) {
+const NewCollections = () => {
   return (
-    <div className="flex items-center justify-between border-b p-2 shadow-sm">
-      <div className="container flex">
-        <div className="flex basis-6/12 items-center justify-start">
-          <Image className="h-8 w-8" src={protokit} alt={"Protokit logo"} />
-          <Separator className="mx-4 h-8" orientation={"vertical"} />
-          <div className="flex grow">
-            <Chain height={blockHeight} />
-          </div>
-        </div>
-        <div className="flex basis-6/12 flex-row items-center justify-end">
-          {/* balance */}
-          {wallet && (
-            <div className="mr-4 flex shrink flex-col items-end justify-center">
-              <div>
-                <p className="text-xs">Your balance</p>
-              </div>
-              <div className="w-32 pt-0.5 text-right">
-                {balanceLoading && balance === undefined ? (
-                  <Skeleton className="h-4 w-full" />
-                ) : (
-                  <p className="text-xs font-bold">{balance} MINA</p>
-                )}
-              </div>
-            </div>
-          )}
-          {/* connect wallet */}
-          <Button loading={loading} className="w-44" onClick={onConnectWallet}>
-            <div>
-              {wallet ? truncateMiddle(wallet, 7, 7, "...") : "Connect wallet"}
-            </div>
-          </Button>
-        </div>
+    <div className='new-collections'>
+      <h1>NEW COLLECTIONS</h1>
+      <hr />
+      <div className="collections">
+        {new_collection.map((item,i)=>{
+            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+        })}
       </div>
     </div>
-  );
+  )
 }
+
+export default NewCollections
--- e-commerce/src/components/RelatedProducts/RelatedProducts.jsx
+++ e-commerce/src/components/RelatedProducts/RelatedProducts.jsx
@@ -1,7 +1,20 @@
-"use client";
-import "reflect-metadata";
-import AsyncPageDynamic from "@/containers/async-page-dynamic";
+import React from 'react'
+import './RelatedProducts.css'
+import data_product from '../Assets/data'
+import Item from '../Item/Item'
 
-export default function Home() {
-  return <AsyncPageDynamic />;
+const RelatedProducts = () => {
+  return (
+    <div className='relatedproducts'>
+        <h1>Related Products</h1>
+        <hr />
+        <div className="relatedproducts-item">
+            {data_product.map((item,i)=>{
+                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+            })}
+        </div>
+    </div>
+  )
 }
+
+export default RelatedProducts
